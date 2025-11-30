bb3749bf2ad526240a65d91537daa1fe846319c1
pRism <52602809+43287@users.noreply.github.com>
2025-11-30 22:29:24 +0800

    fix bugs

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,7 +29,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 export default defineConfig({
 	site: siteConfig.siteURL,
 
-	base: "/",
+	base: "/repo/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
